d149051906c70e0cc0ba09215a17f303884e80a4
ikhsan rahman <ikhsanrahman.h@gmail.com>
2019-10-10 15:42:24 +0700

    update profile page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,122 @@
+<template>
+	<div>
+		<v-container fluid class="transparent" style="background:none;position:fixed;box-shadow:5px 5px 18px #888888">
+      <v-row class="transparent">
+        <v-col>
+          <v-row class="mx-auto" justify="start" >
+            <img src="@/assets/paroha.png" width="120px" height="100px" alt="">
+            <h4 class="mt-10">PT PAROHA TOPAZ SEJAHTERA</h4>
+          </v-row>    
+        </v-col>
+        <v-col class="d-none d-lg-block">
+          <v-row no-gutters>
+            <v-col>
+              <v-row no-gutters>
+              <i class="material-icons">
+                phone
+              </i>
+              <h4>+62-811 1012 117</h4>
+              </v-row>
+            </v-col>
+            <v-col sm="4">
+              <v-row no-gutters>
+                <i class="material-icons">
+                  email
+                </i>
+                <h4>support@paroha.id</h4>
+              </v-row>
+            </v-col>
+            <v-col sm="4">
+              <v-row no-gutters>
+                <i class="material-icons">
+                  access_time
+                </i>
+                <h4>Senin - Jumat : 09.00 sd 18.00 WIB</h4>
+              </v-row>
+            </v-col>
+          </v-row>
+          <hr class="mt-7">
+          <v-row>
+            <v-col>
+              <v-btn text color="primary" @click="home">HOME</v-btn>
+            </v-col>
+            <v-col>
+              <v-btn text color="primary" @click="us=!us">TENTANG KAMI</v-btn>
+              <v-expansion-panels v-show="us">
+              <v-expansion-panel>
+                <v-expansion-panel-header hide-actions @click="profile">Sejarah</v-expansion-panel-header>
+                <v-expansion-panel-header hide-actions @click="profile">Visi Misi</v-expansion-panel-header>
+                <v-expansion-panel-header hide-actions>Tim Kami</v-expansion-panel-header>
+                <v-expansion-panel-header hide-actions>Legalitas</v-expansion-panel-header>
+              </v-expansion-panel>
+            </v-expansion-panels>
+            </v-col>
+            <v-col>
+              <v-btn text color="primary">PRODUK & JASA</v-btn>
+            </v-col>
+            <v-col>
+              <v-btn text color="primary">REKANAN</v-btn>
+            </v-col>
+            <v-col>
+              <v-btn text color="primary">BERITA</v-btn>
+            </v-col>
+            <v-col>
+              <v-btn text color="primary">KONTAK</v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>					
+		</v-container>        
+    <v-container fluid style="position:fixed" class="pr-4 mt-10">
+      <v-row justify="end">
+        <v-app-bar-nav-icon class="d-lg-none blue" @click="show=!show"></v-app-bar-nav-icon>
+        <v-expansion-panels class="d-lg-none" v-show="show">
+          <v-expansion-panel>
+            <v-expansion-panel-header hide-actions @click="home">HOME</v-expansion-panel-header>
+          </v-expansion-panel>
+          <v-expansion-panel>
+            <v-expansion-panel-header>Tentang Kami</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-expansion-panel-header hide-actions @click="profile('profile')">Sejarah</v-expansion-panel-header>
+                <v-expansion-panel-header hide-actions @click="profile('profile')">Visi Misi</v-expansion-panel-header>
+                <v-expansion-panel-header hide-actions>Tim Kami</v-expansion-panel-header>
+                <v-expansion-panel-header hide-actions>Legalitas</v-expansion-panel-header>
+              </v-expansion-panel-content>
+           </v-expansion-panel>
+           <v-expansion-panel>
+            <v-expansion-panel-header hide-actions>PRODUK & JASA</v-expansion-panel-header>
+          </v-expansion-panel>
+          <v-expansion-panel>
+            <v-expansion-panel-header hide-actions>REKANAN</v-expansion-panel-header>
+          </v-expansion-panel>
+          <v-expansion-panel>
+            <v-expansion-panel-header hide-actions>BERITA</v-expansion-panel-header>
+          </v-expansion-panel>
+          <v-expansion-panel>
+            <v-expansion-panel-header hide-actions>KONTAK</v-expansion-panel-header>
+          </v-expansion-panel>
+        </v-expansion-panels>  
+      </v-row>
+    </v-container>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Header',
+		data () {
+			return {
+				show: false,
+				us: false,
+			}
+		},
+		methods: {
+			profile () {
+				this.$router.push({name:'profile'}).catch(err => {})
+			},
+			home () {
+				this.$router.push({name:'home'}).catch(err => {})
+			}
+		}
+	}
+</script>
---
+++ src/components/Profile.vue
@@ -0,0 +1,86 @@
+<template>
+	<div>
+	<navbar/>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<v-container fluid class="profile align-center mt-12">		
+		<v-row no-gutters class="sejarah" >
+			<v-col cols="12" md="8" >
+				<h1 style="color:red;text-decoration:underline;margin-left:21%">TENTANG KAMI</h1>	
+			</v-col>
+		</v-row>
+		<br>
+		<v-row no-gutters class="sejarah" justify="center">
+			<v-col cols="12" md="3" class="ml-12">
+				<img src="@/assets/Tambun.jpg" height="90%" width="90%" alt="">	
+			</v-col>
+			<v-col cols="10" md="6" class="text">
+				<h1>SEJARAH</h1>
+				<h3 style="color:red">
+					Kami fokus bergerak dalam bidang jasa konsultasi dengan mengikuti kebutuhan banyaknya perusahaan dengan tenaga ahli yang diminati
+				</h3>
+				<br>
+				<p>
+					Layanan jasa yang membantu perusahaan anda dalam pengembangan pertanian dan pedesaan, jasa studi, penelitian dan bantuan teknik, keuangan, konsultasi manajemen maupun jasa khusus
+				</p>
+				<br>
+				<p>
+					PT Paroha Topaz Sejahtera berdiri pada tanggal 11 Februari 2019, no.7 berdasarkan akta notaris H.Sukirno, Perusahaan ini fokus bergerak dalam bidang jasa konsultasi dengan mengikuti kebutuhan banyaknya perusahaan dengan tenaga ahli yang diminati
+				</p>
+				<br>
+				<p>
+					PT Paroha Topaz Sejahtera memperoleh izin dengan KADIN dan INKINDO. Sejak bulan Juni 2019, badan usaha yang di kembangkan pengembangan pertanian dan pedesaan, jasa studi, penelitian dan bantuan teknik, keuangan, konsultasi manajemen maupun jasa khusus
+				</p>
+			</v-col>
+		</v-row>
+		<v-row class="visi-misi">
+			<v-col>
+				<h2>VISI</h2>
+				<p>Menjadi Perusahaan nasional yang kompetitif, andal dan terpercaya di bidang pemetaan, inspeksi, sertifikasi, konsultasi dan pelatihan</p>
+			</v-col>
+		</v-row>
+		<v-row class="visi-misi">
+			<v-col>
+				<h2>MISI</h2>
+				<p>
+					Menciptakan Nilai Ekonomi kepada para pemangku kepentingan, terutama pelanggan melalui layanan jasa inspeksi, pemetaan, sertifikasi, konsultasi serta jasa terkait lainnya untuk menjamin kepastian berusaha
+				</p>
+			</v-col>
+		</v-row>
+	</v-container>
+	</div>
+</template>
+
+<script>
+	import Navbar from './Navbar'
+	export default {
+		name: 'TentangKami',
+		components: {
+			Navbar
+		},
+		data () {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.profile {
+		height: 100%;
+	}
+	.visi-misi {
+		margin-left: 10%;
+	}
+	.sejarah {
+		margin-left: 5%;
+	}
+	.image {
+		margin-left:1%;
+	}
+</style>
